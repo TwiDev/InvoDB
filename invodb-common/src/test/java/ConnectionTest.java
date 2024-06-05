1d28fd864a94e387bbf1da076e66d623a4e37ff2
twidev <twidev5@gmail.com>
2024-06-05 20:34:40 +0200

    update example

--- invodb-common/src/test/java/ConnectionTest.java
+++ invodb-common/src/test/java/ConnectionTest.java
@@ -1,12 +1,7 @@
-import ch.twidev.invodb.bridge.contexts.SearchDictionary;
 import ch.twidev.invodb.bridge.contexts.SearchFilterType;
-import ch.twidev.invodb.common.query.InvoQuery;
-import ch.twidev.invodb.common.query.operations.search.*;
 
 import java.util.HashMap;
 import java.util.logging.Logger;
-
-import static ch.twidev.invodb.common.query.operations.search.SearchFilter.*;
 
 public class ConnectionTest {
 
@@ -41,44 +36,6 @@ public class ConnectionTest {
         } catch (DriverBuilderException | DriverConnectionException e) {
             throw new RuntimeException(e);
         }*/
-
-        InvoQuery.find("main")
-                .where(and(
-                        eq("user_name", "TwiDev"),
-                        not_eq("user_id", 2)))
-                .attribute("user_email")
-                .run(null, (resultSet) -> {
-
-                });
-
-
-        SearchDictionary searchDictionary = new SearchDictionary(){{
-            put(SearchFilterType.ALL, new SearchCompositeParameter("*"));
-            put(SearchFilterType.AND, new SearchCompositeParameter("AND"));
-            put(SearchFilterType.OR, new SearchCompositeParameter("OR"));
-            put(SearchFilterType.EQUAL, new SearchFieldParameter() {
-                @Override
-                public String parse(String key) {
-                    return key + " = ?";
-                }
-            });
-            put(SearchFilterType.NOT_EQUAL, new SearchFieldParameter() {
-                @Override
-                public String parse(String key) {
-                    return key + " = ?";
-                }
-            });
-        }};
-
-        SearchFilter filter = and(
-                SearchFilter.eq("name", "John"),
-                SearchFilter.or(
-                        SearchFilter.eq("age", 30),
-                        SearchFilter.not_eq("city", "New York")
-                )
-        );
-
-        System.out.println(filter.toQuery(searchDictionary, null)); /* OUTPUT : (name = 'John' AND (age = '30' OR city = 'New York'))*/
 
 
 
